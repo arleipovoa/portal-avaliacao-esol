268174350e5f29fed032761b871183837ce53bd3
Árlei Póvoa <arleipovoa@gmail.com>
2026-04-30 21:40:31 -0300

    fix(routing): adicionar Router da wouter na raiz (preserva 813e114)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink, TRPCClientError } from "@trpc/client";
 import { createRoot } from "react-dom/client";
 import superjson from "superjson";
+import { Router } from "wouter";
 import App from "./App";
 import "./index.css";
 
@@ -54,7 +55,9 @@ const trpcClient = trpc.createClient({
 createRoot(document.getElementById("root")!).render(
   <trpc.Provider client={trpcClient} queryClient={queryClient}>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </QueryClientProvider>
   </trpc.Provider>
 );
